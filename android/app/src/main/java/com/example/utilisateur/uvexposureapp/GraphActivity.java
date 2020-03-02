9c9f56ac8f8841f594707bdae4b94eb977889618
DPigeon <D_Pig@encs.concordia.ca>
2020-03-02 15:17:28 -0500

    #12: Example of a graph with some title

--- android/app/src/main/java/com/example/utilisateur/uvexposureapp/GraphActivity.java
+++ android/app/src/main/java/com/example/utilisateur/uvexposureapp/GraphActivity.java
@@ -5,25 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+/*
+* An activity that displays the UV exposure graph depending on the time chosen.
+* Data from the database will have to be fetched in order to view them.
+*/
+
 public class GraphActivity extends AppCompatActivity {
+    GraphView graph;
+    LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
+        graph = findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        setupGraph();
+    }
+
+    protected void setupGraph() {
+        double x, y;
+        x = -5.0;
+
+        for (int i = 0; i < 500; i++) {
+            x = x + 0.04;
+            y = Math.tanh(x); // A simple example
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
         graph.addSeries(series);
 
+        // Legend
+        series.setTitle("UV Exposure Against Time");
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+    }
+
+    protected void fetchData() {
+        // Will be used to fetch some UV exposure points <time, UV>
     }
 }
