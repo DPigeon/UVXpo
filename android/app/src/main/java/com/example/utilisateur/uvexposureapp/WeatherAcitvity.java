0cee67145fa84611551d67a34ed7bfbc2532af98
Pirey96 <pirey8888@gmail.com>
2020-03-04 02:12:02 -0500

    WeatherAPI
    update

--- android/app/src/main/java/com/example/utilisateur/uvexposureapp/WeatherAcitvity.java
+++ android/app/src/main/java/com/example/utilisateur/uvexposureapp/WeatherAcitvity.java
@@ -3,6 +3,7 @@ package com.example.utilisateur.uvexposureapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -14,32 +15,38 @@ import java.util.concurrent.ExecutionException;
 
 public class WeatherAcitvity extends AppCompatActivity {
     protected TextView Weather;
-    protected TextView City;
+    protected EditText City;
     protected TextView Country;
     protected TextView Status;
-
+    static String cityName = "London";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_acitvity);
-        //setupUI();
-        //String content;
+        String main = "";
+        String desc = "";
+        String temps = "";
+        setupUI(main,desc,temps);
         Weather weather = new Weather();
         try {
             String content = weather.execute(weatherInfo()).get();
-          //  Log.i("contentData", content);
+             //Log.i("contentData", content);
             JSONObject jsonObject = new JSONObject(content);
             String weatherData = jsonObject.getString("weather");
             //Log.i("WeatherData",weatherData);
             JSONArray array = new JSONArray(weatherData);
 
-            String main;
-            String desc;
+
+            String temperature = "";
+            temperature = jsonObject.getString("main");
             for (int i = 0;i<array.length();i++){
                 JSONObject weatherPt = array.getJSONObject(i);
                 main = weatherPt.getString("main");
-                desc = weatherPt.getString("desc");
+                desc = weatherPt.getString("description");
+
             }
+            JSONObject temp = new JSONObject(temperature);
+            temps = temp.getString("temp");
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -48,20 +55,21 @@ public class WeatherAcitvity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
     }
 
-    void setupUI() {
+    void setupUI(String main,String desc,String temps) {
         Weather = findViewById(R.id.Weather);
         City = findViewById(R.id.City);
         Country = findViewById(R.id.weather);
         Status = findViewById(R.id.Status);
-
-        Weather.setText("30 C");
+        cityName = City.getText().toString();
+        Weather.setText("Status: "+ main +"\n"+"Description: "+desc+"\n"+"Temperature: "+temps);
 
     }
 
     public String weatherInfo() {
-        String URL = "https://openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22";
+        String URL = "https://openweathermap.org/data/2.5/weather?q="+cityName+","+"uk"+"&appid=b6907d289e10d714a6e88b30761fae22";
         return URL;
 
 
