e4338cf2a1f6d4b74ee16ea8d877315ac8586a77
DPigeon <D_Pig@encs.concordia.ca>
2020-03-06 13:48:51 -0500

    Removing the time

--- android/app/src/main/java/com/example/faq/MainActivity.java
+++ android/app/src/main/java/com/example/faq/MainActivity.java
@@ -6,38 +6,28 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    String URL = "https://www.ccohs.ca/oshanswers/phys_agents/ultravioletradiation.html?fbclid=IwAR05zwUhYrQqcc0bNr-nSeWcbN7J1LUsjgW3K7Bs5oT49s_O9XrgfFpZybY";
+    Button faqButton;
 
-
-    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.textView);
-        /* Set button to display time */
-        Calendar calendar= Calendar.getInstance();
-        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("hh:mm:ss a");
-        String dateTime= simpleDateFormat.format(calendar.getTime());
-        textView.setText(dateTime);
-
+        faqButton = findViewById(R.id.faqButton);
+        faqButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFaqWebsite(view);
+            }
+        });
     }
 
 /* Linking website to FAQ button */
-
-    public void faq(View view){
-
-        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ccohs.ca/oshanswers/phys_agents/ultravioletradiation.html?fbclid=IwAR05zwUhYrQqcc0bNr-nSeWcbN7J1LUsjgW3K7Bs5oT49s_O9XrgfFpZybY"));
+    public void openFaqWebsite(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
         startActivity(browserIntent);
-
-
-
     }
-
-
 }
