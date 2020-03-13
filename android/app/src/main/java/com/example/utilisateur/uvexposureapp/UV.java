e4c558124f55f92b4685ca71af80031d3fde9858
t-akther <60326979+t-akther@users.noreply.github.com>
2020-03-13 16:30:17 -0400

    uv.java file

---
+++ android/app/src/main/java/com/example/utilisateur/uvexposureapp/UV.java
@@ -0,0 +1,38 @@
+package com.example.uv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import java.time;
+
+
+public class UV {
+
+    private double uv;
+    private LocalDateTime date;
+
+
+    public UV (double uv, LocalDateTime date){
+
+        this.uv= uv;
+        this.date=date;
+    }
+    /*set UV value */
+    public void setUv(double uv) {
+        this.uv = uv;
+    }
+
+
+    /*get UV value*/
+
+    public double getUv() {
+        return uv;
+    }
+    /*set current time and date */
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+    /*get current time and date */
+    public LocalDateTime getDate() {
+        return date;
+    }
+}
