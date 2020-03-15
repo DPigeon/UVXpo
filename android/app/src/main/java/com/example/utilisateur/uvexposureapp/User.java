ba151c274d079e2df0291a9cee924d46f1f7578c
nmoncada96 <60986679+nmoncada96@users.noreply.github.com>
2020-03-15 19:41:07 -0400

    Merge branch 'master' of https://github.com/DPigeon/COEN390-ELEC390-Project

---
+++ android/app/src/main/java/com/example/utilisateur/uvexposureapp/User.java
@@ -0,0 +1,81 @@
+package com.example.utilisateur.uvexposureapp;
+
+import java.util.ArrayList;
+
+public class User {
+    private String username;
+    private String password;
+    private int age;
+    private ArrayList<UV> uvData; // A user will have stored uv values tracked
+    private int skin;
+    private boolean notifications;
+
+    public User (String username, String password, int age, ArrayList<UV> uv, int skin, boolean notifications) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.uvData = uv;
+        this.skin = skin;
+        this.notifications = notifications;
+    }
+
+    /* set Username*/
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /* Get Username*/
+    public String getUsername() {
+        return username;
+    }
+
+    /* Set Password*/
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /* Get Password*/
+    public String getPassword() {
+        return password;
+    }
+
+    /* Set age*/
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /* Get age*/
+    public int getAge() {
+        return age;
+    }
+
+    /* set uv value*/
+    public void setUv(UV uv) {
+        this.uvData.add(uv);
+    }
+
+    /* get uv values*/
+    public ArrayList<UV> getUvData() {
+        return uvData;
+    }
+
+    /* set Skin type*/
+    public void setSkin(int skin) {
+        this.skin = skin;
+    }
+
+    /* get Skin type*/
+    public int getSkin() {
+        return skin;
+    }
+
+    /* Set notifications*/
+    public void setNotifications(boolean notifications) {
+        this.notifications = notifications;
+    }
+
+    /* Get notifications*/
+    public boolean getNotifications() {
+        return notifications;
+    }
+}
